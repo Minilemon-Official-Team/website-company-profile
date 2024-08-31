8b990a402d07823416fd8670614b4c31c86dd62f
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2024-08-31 13:33:27 +0700

    fixed style homepage, change assets character

--- components/footer.jsx
+++ components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <>
       <DividerBlack className="bg-[#1b1b1b]" />
-      <div className="flex flex-col justify-center bg-[#272727] items-center py-2">
+      <div className="flex flex-col justify-center bg-[#272727] items-center py-4">
         <Image src={LogoBrand} alt="logo" width={100} height={100} />
         <p className="text-xs text-[#868686]">Copyright 2020 - {currrentYear} | Minilemon | All Rights Reserved</p>
       </div>
