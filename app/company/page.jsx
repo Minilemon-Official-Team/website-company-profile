fb3e0b1fa094583983c3ac8b1cb7df17108d6cd9
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2024-09-18 12:48:09 +0700

    fixed title, placeholder partners

--- app/company/page.jsx
+++ app/company/page.jsx
@@ -4,6 +4,12 @@ import { luckiest_guy } from "../fonts";
 import { Placeholder } from "placeholder";
 import Image from "next/image";
 import { specialPartners } from "../../data/specialPartners";
+import VisiTitle from "@/public/title-picture/visi-1.png";
+import MisiTitle from "@/public/title-picture/misi-1.png";
+import FounderTitle from "@/public/title-picture/the-founder-1.png";
+import CoFounderTitle from "@/public/title-picture/co-founder-1.png";
+import SpecialPartnerTitle from "@/public/title-picture/special-partner-1.png";
+import PTMinilemonNusantaraTitle from "@/public/title-picture/pt-minilemon-nusantara.png";
 
 const CompanyPage = () => {
   return (
@@ -11,15 +17,12 @@ const CompanyPage = () => {
       {/* PT Minilemon Indonesia */}
       <div className="bg-[#d3def4] py-20">
         <div className="container flex flex-col gap-y-6 p-6">
-          <h2
-            className={cn(
-              luckiest_guy.className,
-              "items-center text-wrap bg-gradient-to-b from-[#fdfe28] from-10% via-[#ffa136] via-40% to-[#f66708] to-80% bg-clip-text text-5xl text-transparent md:text-6xl lg:text-7xl",
-              "drop-shadow-[0px_4px_2px_rgba(129,40,86,1)]",
-            )}
-          >
-            PT. Minilemon Nusantara
-          </h2>
+          <div className="flex w-full justify-center">
+            <Image
+              src={PTMinilemonNusantaraTitle}
+              alt="PT Minilemon Nusantara"
+            />
+          </div>
           <div className="flex flex-col gap-y-6 leading-loose tracking-widest">
             <p>
               PT. Minilemon Nusantara adalah perusahaan yang berdomisili di
@@ -48,15 +51,10 @@ const CompanyPage = () => {
       {/* Visi dan Misi */}
       <div className="bg-[#2f2f2f] py-20">
         <div className="container flex flex-col gap-y-6 p-6">
-          <h2
-            className={cn(
-              luckiest_guy.className,
-              "items-center text-wrap bg-gradient-to-b from-[#fdfe28] from-10% via-[#ffa136] via-40% to-[#f66708] to-80% bg-clip-text text-center text-5xl text-transparent md:text-6xl lg:text-7xl",
-            )}
-          >
-            Visi
-          </h2>
-          <p className="leading-relaxed text-[#d1d9e6]">
+          <div className="flex w-full justify-center">
+            <Image src={VisiTitle} alt="Visi dan Misi" />
+          </div>
+          <p className="leading-loose tracking-widest text-[#d1d9e6]">
             Sebagai anak bangsa yang baik dan cinta tanah air, PT Minilemon
             Nusantara mengambil tanggung jawab untuk berkontribusi memberikan
             edukasi dan harapan untuk kemajuan bangsa.
@@ -67,15 +65,10 @@ const CompanyPage = () => {
             membawa nilai - nilai kebaikan dan budaya yang merupakan warisan
             yang selalu kita banggakan.
           </p>
-          <h2
-            className={cn(
-              luckiest_guy.className,
-              "items-center text-wrap bg-gradient-to-b from-[#fdfe28] from-10% via-[#ffa136] via-40% to-[#f66708] to-80% bg-clip-text text-center text-5xl text-transparent md:text-6xl lg:text-7xl",
-            )}
-          >
-            Misi
-          </h2>
-          <ol className="list-decimal leading-relaxed text-[#aab3c3]">
+          <div className="flex w-full justify-center">
+            <Image src={MisiTitle} alt="Misi" />
+          </div>
+          <ol className="ml-4 list-decimal leading-loose tracking-widest text-[#aab3c3]">
             <li className="text-[#d1d9e6]">
               Menciptakan produk kreatif dan eduktif yang bekelanjutan
             </li>
@@ -93,134 +86,103 @@ const CompanyPage = () => {
 
       {/* Founder */}
       <div className="flex flex-row bg-[#a4bddc] py-12">
-        <div className="container flex flex-col gap-x-6 px-8 md:flex-row lg:px-0">
-          <div className="flex basis-4/12 flex-col items-center justify-center">
-            <h2
-              className={cn(
-                luckiest_guy.className,
-                "mb-4 items-center text-wrap bg-gradient-to-b from-[#fdfe28] from-10% via-[#ffa136] via-40% to-[#f66708] to-80% bg-clip-text text-4xl text-transparent md:hidden",
-                "drop-shadow-[0px_5px_0px_rgba(2,43,125,1)]",
-              )}
-            >
-              The Founder
-            </h2>
-            <Placeholder
-              width="350"
-              height="500"
-              background="#333"
-              color="#fff"
-            />
+        <div className="container flex flex-col gap-6 px-8 lg:px-0">
+          <div className="flex w-full justify-center lg:justify-start">
+            <Image src={FounderTitle} alt="The Founder" />
           </div>
-          <div className="flex basis-8/12 flex-col gap-y-6 leading-loose tracking-wider text-[#1b1b1d]">
-            <div>
-              <h2
-                className={cn(
-                  luckiest_guy.className,
-                  "hidden items-center text-wrap bg-gradient-to-b from-[#fdfe28] from-10% via-[#ffa136] via-40% to-[#f66708] to-80% bg-clip-text text-transparent md:block md:text-6xl lg:text-7xl",
-                  "drop-shadow-[0px_5px_0px_rgba(2,43,125,1)]",
-                )}
-              >
-                The Founder
-              </h2>
+          <div className="flex flex-col gap-6 lg:flex-row">
+            <div className="flex basis-5/12 items-center justify-center lg:justify-start">
+              <Placeholder
+                width="400"
+                height="570"
+                background="#333"
+                color="#fff"
+              />
             </div>
-            <p>
-              <b>Reno Halsamer</b> Adalah Pria yang lahir dan tinggal di
-              Surabaya Indoensia. Sebagai seorang bisnisman dan penggiat Museum
-              seni khususnya Topeng Indonesia, Sejak tahun 2015 di Surabaya,
-              ditengah situasi yang dirasa mundurnya kecintaan masyarakat
-              terhadap seni dan budaya, Reno Halsamer mencetuskan ide untuk
-              menciptakan karakter animasi dengan mengkombinasikan topeng dan
-              buah lemon. Selain topeng merupakan karya seni yang dibuat
-              orisinil dari leluhur bangsa Indonesia, topeng juga diharapkan
-              mampu mewakili wajah karakter masyarakan Indonesia
-            </p>
-            <p>
-              Karakter animasi minilemon diharapkan menjadi figur baik dan
-              menjadi teladan terutama untuk anak-anak Indonesia, sehingga buah
-              lemon yang dikenal banyak manfaatnya, sangat cocok dipadukan
-              dengan topeng, untuk menjadi "THE GOOD CHARACTER" dengan "WAJAH
-              INDOENSIA". Dengan idenya yang brilian, "BAPAK MINILEMON"
-              merupakan julukan yang pantas dilekatkan dengan Reno Halsamer.
-            </p>
+            <div className="flex basis-7/12 flex-col gap-y-6 leading-loose tracking-wider text-[#1b1b1d]">
+              <p>
+                <b>Reno Halsamer</b> Adalah Pria yang lahir dan tinggal di
+                Surabaya Indoensia. Sebagai seorang bisnisman dan penggiat
+                Museum seni khususnya Topeng Indonesia, Sejak tahun 2015 di
+                Surabaya, ditengah situasi yang dirasa mundurnya kecintaan
+                masyarakat terhadap seni dan budaya, Reno Halsamer mencetuskan
+                ide untuk menciptakan karakter animasi dengan mengkombinasikan
+                topeng dan buah lemon. Selain topeng merupakan karya seni yang
+                dibuat orisinil dari leluhur bangsa Indonesia, topeng juga
+                diharapkan mampu mewakili wajah karakter masyarakan Indonesia
+              </p>
+              <p>
+                Karakter animasi minilemon diharapkan menjadi figur baik dan
+                menjadi teladan terutama untuk anak-anak Indonesia, sehingga
+                buah lemon yang dikenal banyak manfaatnya, sangat cocok
+                dipadukan dengan topeng, untuk menjadi "THE GOOD CHARACTER"
+                dengan "WAJAH INDOENSIA". Dengan idenya yang brilian, "BAPAK
+                MINILEMON" merupakan julukan yang pantas dilekatkan dengan Reno
+                Halsamer.
+              </p>
+            </div>
           </div>
         </div>
       </div>
 
       {/* Co Founder */}
       <div className="flex flex-row bg-[#d3def4] py-12">
-        <div className="container flex flex-col gap-x-6 px-8 md:flex-row lg:px-0">
-          <div className="flex basis-4/12 flex-col items-center justify-center">
-            <h2
-              className={cn(
-                luckiest_guy.className,
-                "mb-4 items-center text-wrap bg-gradient-to-b from-[#fdfe28] from-10% via-[#ffa136] via-40% to-[#f66708] to-80% bg-clip-text text-4xl text-transparent md:hidden",
-                "drop-shadow-[0px_5px_0px_rgba(2,43,125,1)]",
-              )}
-            >
-              Co Founder, CEO
-            </h2>
-            <Placeholder
-              width="350"
-              height="500"
-              background="#333"
-              color="#fff"
-            />
+        <div className="container flex flex-col gap-6 px-8 lg:px-0">
+          <div className="flex w-full justify-center lg:justify-start">
+            <Image src={CoFounderTitle} alt="Co Founder" />
           </div>
-          <div className="flex basis-8/12 flex-col gap-y-6 leading-loose tracking-wider text-[#1b1b1d]">
-            <div>
-              <h2
-                className={cn(
-                  luckiest_guy.className,
-                  "hidden items-center text-wrap bg-gradient-to-b from-[#fdfe28] from-10% via-[#ffa136] via-40% to-[#f66708] to-80% bg-clip-text text-transparent md:block md:text-6xl lg:text-7xl",
-                  "drop-shadow-[0px_5px_0px_rgba(2,43,125,1)]",
-                )}
-              >
-                Co Founder, CEO
-              </h2>
+          <div className="flex flex-col gap-6 lg:flex-row">
+            <div className="flex basis-5/12 items-center justify-center lg:justify-start">
+              <Placeholder
+                width="400"
+                height="570"
+                background="#333"
+                color="#fff"
+              />
             </div>
-            <p>
-              <b>Heriyadi Natawijaya</b> lahir di Palembang dan tinggal di Depok
-              Indonesia, adlaah Seorang Sutradara iklan, Video Klik dan Animasi
-              Sejak 2009. Heriyadi Natawijaya juga sebagai Pendiri Perakaria &
-              IDsains sebagai Pusat Komunitas Kreatif dan produksi seni di
-              Depok, Indonesia.
-            </p>
-            <p>
-              Karirnya di dunia Seni dan Perfilman membuatnya bertemu Reno
-              Halsamer sebagai sosok pencetus Minilemon. Mempunyai Visi Misi
-              yang sama untuk kemudian menjadikan minilemon sebagai produk
-              Kreatif, edukatif dengan nilai-nilai budaya.
-            </p>
-            <p>
-              Pengalaman panjang sebagai kepala Seni, dan kecintaannya dengan
-              teknologi, membuat Heriyadi Natawijaya banyak menciptakan gagasan
-              dan putusan yang berpihak kepada estetika dan budaya kerja yang
-              tech savvy, menyenangkan, transparan dan terukur. dengan pemikiran
-              dan Kepiawayannya "Paman Minilemon" adalah julukan yang cocok
-              untuk Heriyadi Natawijaya.
-            </p>
+            <div className="flex basis-7/12 flex-col gap-y-6 leading-loose tracking-wider text-[#1b1b1d]">
+              <p>
+                <b>Heriyadi Natawijaya</b> lahir di Palembang dan tinggal di
+                Depok Indonesia, adlaah Seorang Sutradara iklan, Video Klik dan
+                Animasi Sejak 2009. Heriyadi Natawijaya juga sebagai Pendiri
+                Perakaria & IDsains sebagai Pusat Komunitas Kreatif dan produksi
+                seni di Depok, Indonesia.
+              </p>
+              <p>
+                Karirnya di dunia Seni dan Perfilman membuatnya bertemu Reno
+                Halsamer sebagai sosok pencetus Minilemon. Mempunyai Visi Misi
+                yang sama untuk kemudian menjadikan minilemon sebagai produk
+                Kreatif, edukatif dengan nilai-nilai budaya.
+              </p>
+              <p>
+                Pengalaman panjang sebagai kepala Seni, dan kecintaannya dengan
+                teknologi, membuat Heriyadi Natawijaya banyak menciptakan
+                gagasan dan putusan yang berpihak kepada estetika dan budaya
+                kerja yang tech savvy, menyenangkan, transparan dan terukur.
+                dengan pemikiran dan Kepiawayannya "Paman Minilemon" adalah
+                julukan yang cocok untuk Heriyadi Natawijaya.
+              </p>
+            </div>
           </div>
         </div>
       </div>
       {/* Special Partner */}
-      <div className="bg-[#fbe5db] py-20">
+      <div className="hidden bg-[#fbe5db] py-20 lg:flex">
         <div className="container">
-          <h2
-            className={cn(
-              luckiest_guy.className,
-              "mb-10 items-center text-wrap bg-gradient-to-b from-[#fdfe28] from-10% via-[#ffa136] via-40% to-[#f66708] to-80% bg-clip-text text-center text-5xl text-transparent md:text-6xl lg:text-7xl",
-            )}
-          >
-            Special Partner
-          </h2>
+          <div className="mb-16 flex w-full justify-center">
+            <Image src={SpecialPartnerTitle} alt="Special Partner" />
+          </div>
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
             {specialPartners.map((partner, index) => (
               <div key={index} className="text-center">
-                <Image
-                  src={partner.image}
-                  alt={partner.name}
-                  className="mx-auto"
-                />
+                <div className="flex justify-center">
+                  <Placeholder
+                    width="200"
+                    height="250"
+                    background="#333"
+                    color="#fff"
+                  />
+                </div>
                 <p className="mt-4">{partner.name}</p>
                 <p className="text-sm font-semibold">{partner.role}</p>
               </div>
