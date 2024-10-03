e43d456a468bd9981edef49bd33c1ac708a1e7f1
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2024-10-04 00:03:05 +0700

    revamp about page (#73)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,33 @@
 import React from "react";
+import Image from "next/image";
+
 import IdeaTitle from "@/public/title-picture/idea-1.png";
 import MessageTitle from "@/public/title-picture/message-1.png";
 import TreatmentTitle from "@/public/title-picture/treatment-1.png";
 import ConceptTitle from "@/public/title-picture/concept-1.png";
 import PsychographicTitle from "@/public/title-picture/psychographic-1.png";
-import Image from "next/image";
+import BackgroundImage from "@/public/background/about.png";
+import DividerBlue from "@/components/ui/divider/divider-blue";
 
 const AboutPage = () => {
   return (
     <div className="flex flex-col">
       {/* Idea */}
-      <div className="bg-[#d3def4] py-20">
-        <div className="container flex flex-col justify-center gap-y-8 p-6">
-          <div>
+      <div className="relative h-full justify-center overflow-hidden py-20">
+        <Image
+          src={BackgroundImage}
+          alt="Background"
+          quality={100}
+          fill
+          sizes="100vw"
+          className="absolute inset-0 z-[-1]"
+          objectFit="cover"
+        />
+        <div className="container flex flex-col justify-center gap-y-6">
+          <div className="mt-2">
             <Image src={IdeaTitle} alt="Idea" />
           </div>
-          <div className="flex flex-col gap-y-6 leading-loose tracking-widest">
+          <div className="flex flex-col gap-y-6 font-semibold leading-loose tracking-widest text-[#c5cce2]">
             <p>
               Berawal dari cinta dan bangga akan warisan budaya khusunya topeng
               yang merupakan penutup wajah dari setiap Suku di Indonesia.
@@ -35,16 +47,17 @@ const AboutPage = () => {
             </p>
           </div>
         </div>
+        <DividerBlue className="absolute bottom-0 bg-transparent" />
       </div>
 
       {/* Message */}
-      <div className="bg-[#d3eff3] py-20">
+      <div className="bg-[#d5ddf4] py-10">
         <div className="container flex flex-col justify-center gap-y-6 p-6 text-end">
           <div className="flex justify-end">
             <Image src={MessageTitle} alt="Message" />
           </div>
           <div className="flex flex-col gap-y-6 leading-loose tracking-widest">
-            <p>
+            <p className="font-medium text-[#2c2d2f]/90">
               minimnya hiburan yang mengedepankan kualitas dan edukasi kepada
               anak-anak Indonesia, Minilemon berharap mampu menjadi idola baru,
               menanamkan nilai-nilai positif di banyak platform dan gadget yang
@@ -55,12 +68,12 @@ const AboutPage = () => {
       </div>
 
       {/* Treatment */}
-      <div className="bg-[#d3f4df] py-20">
+      <div className="bg-[#1b1b1b] py-12">
         <div className="container flex flex-col justify-center gap-y-6 p-6 text-center">
           <div className="flex justify-center">
             <Image src={TreatmentTitle} alt="Treatment" />
           </div>
-          <p className="flex flex-col gap-y-6 leading-loose tracking-widest">
+          <p className="flex flex-col gap-y-6 leading-loose tracking-wider text-[#c2cbde]">
             Sadar akan persoalan-persoalan besar dan mendasar, PT Minilemon
             Nusantara berinisitatif membuat produk-produk anak yang berupa
             tontonan dan permainan, dengan targe usia 3-7 tahun.
@@ -69,12 +82,12 @@ const AboutPage = () => {
       </div>
 
       {/* Concept */}
-      <div className="bg-[#f0f4d3] py-20">
-        <div className="container flex flex-col justify-center gap-y-6 p-6 text-start">
-          <div>
+      <div className="bg-[#d6f4df] py-16">
+        <div className="container flex flex-col justify-center gap-y-6 text-start">
+          <div className="mt-2">
             <Image src={ConceptTitle} alt="Concept" />
           </div>
-          <p className="flex flex-col gap-y-6 leading-loose tracking-widest">
+          <p className="flex flex-col pb-6 leading-loose tracking-wider">
             Minilemon adalah kisah 6 sahabat sepermainan yang berasal dari suku
             berbeda-beda yaitu Jawa, Sunda, Papua, Tionghoa, Batak dan Bali.
             meskipun berbeda Minilemon selalu mengedepankan sikap toleransi dan
@@ -85,7 +98,7 @@ const AboutPage = () => {
       </div>
 
       {/* Psychographic */}
-      <div className="bg-[#f4d8d4] py-20">
+      <div className="bg-[#f4d8d4] py-16">
         <div className="container flex flex-col justify-center gap-y-6 p-6 text-end">
           <div className="flex justify-end">
             <Image src={PsychographicTitle} alt="Psychographic" />
--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import React from "react";
 
 import LogoBrand from "../public/logo.png";
-import DividerBlack from "./ui/divider-black";
+import DividerBlack from "./ui/divider/divider-black";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
@@ -13,9 +13,10 @@ const Footer = () => {
   const pathname = usePathname();
 
   return (
-    <>
+    <div className="relative">
       <DividerBlack
         className={cn(
+          "absolute top-0",
           pathname === "/" ? "bg-[#f4d8d4] lg:bg-[#1b1b1b]" : "",
           pathname === "/about" ? "bg-[#f4d8d4]" : "",
           pathname === "/story" ? "bg-[#78602f]" : "",
@@ -25,13 +26,13 @@ const Footer = () => {
           pathname === "/contact" ? "bg-[#d2d5f4]" : "",
         )}
       />
-      <div className="flex flex-col items-center justify-center gap-3 bg-[#272727] py-3">
+      <div className="flex flex-col items-center justify-center gap-3 bg-[#272727] py-3 pt-8">
         <Image src={LogoBrand} alt="logo" width={80} height={80} />
         <p className="text-xs font-semibold tracking-wider text-[#868686]">
           Copyright 2020 - {currrentYear} | Minilemon | All Rights Reserved
         </p>
       </div>
-    </>
+    </div>
   );
 };
 
