1d94f38a93676ba55bfa3da0e9390ac60045ff75
GhazaRiZ <74690464+LevianthProject@users.noreply.github.com>
2024-11-04 17:48:20 +0700

    Fix minor char

--- app/(mobile-screen)/character-section.tsx
+++ app/(mobile-screen)/character-section.tsx
@@ -45,7 +45,7 @@ function CharacterSection() {
       </div>
       {/* Change character picture and status */}
       <div className="flex justify-center bg-[#dedede]">
-        <div className="max-w-screen-300 400:max-w-screen-400 640:max-w-screen-640 450:max-w-screen-450 px-6 py-12">
+        <div className="max-w-screen-360 400:max-w-screen-400 640:max-w-screen-640 450:max-w-screen-450 p-6">
           <Splide
             options={{
               type: "loop",
@@ -61,12 +61,12 @@ function CharacterSection() {
           >
             {charactersData.map((character) => (
               <SplideSlide key={character.id}>
-                <div className="max-w-screen-300 400:max-w-screen-400 640:max-w-screen-640 450:max-w-screen-450 flex h-full flex-col gap-y-4">
+                <div className="max-w-screen-360 400:max-w-screen-400 640:max-w-screen-640 450:max-w-screen-450 flex h-full flex-col gap-y-4">
                   <div className="flex flex-1 basis-1/2 flex-row">
                     <div className="basis-1/2">
                       <Image src={character.image} alt={character.fullName} />
                     </div>
-                    <div className="flex basis-1/2 flex-col">
+                    <div className="flex basis-1/2 gap-3 flex-col">
                       <div>
                         <Image
                           src={character.namePicture}
@@ -74,7 +74,7 @@ function CharacterSection() {
                           className="w-[100px]"
                         />
                       </div>
-                      <div className="flex flex-col">
+                      <div className="flex gap-2 flex-col">
                         <Slider value={character.strong} slider="bg-[#ededed]">
                           Strong
                         </Slider>
